5824c9da909a88fa7215c3be2a3bb21a1ba92099
Arvin-Coxcomb <76547948+Arvin-Coxcomb@users.noreply.github.com>
2024-04-15 20:12:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("Rocky");
+const arrows = document.querySelectorAll(".arrow");
+const movieList = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i)=>{
+    
+    const itemNumber = movieList[i].querySelectorAll("img").length;
+    let clickCounter = 0; 
+    arrow.addEventListener("click",()=>{
+        const ratio = Math.floor(window.innerWidth/270);
+        clickCounter++;
+        if(itemNumber - (4+clickCounter) + (4-ratio)>=0){
+        movieList[i].style.transform = `translateX(${movieList[i].computedStyleMap().get("transform")[0].x.value-300}px)`}
+            else{
+                movieList[i].style.transform = "translateX(0)";
+                clickCounter=0;
+            }
+        
+    });
+    console.log(Math.floor(window.innerWidth/270));
+        
+   
+});
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle");
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active");
+    })
+    ball.classList.toggle("active");
+})
+
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+        hiddenElements.forEach((el)=> observer.observe(el));
